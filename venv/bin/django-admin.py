aa7dbd68efb7c7f745c2dd680fdce8ed1a3c353d
runwang <run.wang@logicsolutions.com>
2018-11-12 22:40:51 -0500

    Add existing heritagesites code to the repo.

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/runwang/umichCourse/18fall/SI664/heritagesites/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
